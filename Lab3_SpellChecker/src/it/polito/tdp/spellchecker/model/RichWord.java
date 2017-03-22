d6748a20da37febc8cf0d317fa799de15ec71f63
alberto <alberto@172.20.233.205>
2017-03-22 10:50:29 +0100

    g

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/RichWord.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/RichWord.java
@@ -1,5 +1,13 @@
 package it.polito.tdp.spellchecker.model;
 
 public class RichWord {
-
+private boolean corretto;
+private String word;
+public RichWord(String word) {
+	super();
+	this.word = word;
+	this.corretto=false;
+}
+	
+	
 }
