2415aec1af9e2f7b5a702cfd926e131382f7d50c
alberto <alberto@172.20.233.205>
2017-03-22 10:42:16 +0100

    tu

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,67 @@
 package it.polito.tdp.spellchecker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class SpellCheckerController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> cmbboxLanguage;
+
+    @FXML
+    private TextArea txtInsert;
+
+    @FXML
+    private Button btnCheck;
+
+    @FXML
+    private TextArea txtWrong;
+
+    @FXML
+    private Label lblError;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void doCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbboxLanguage != null : "fx:id=\"cmbboxLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtInsert != null : "fx:id=\"txtInsert\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnCheck != null : "fx:id=\"btnCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtWrong != null : "fx:id=\"txtWrong\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert lblError != null : "fx:id=\"lblError\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert lblTime != null : "fx:id=\"lblTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+
+        
+        
+        cmbboxLanguage.getItems().addAll("English","Italian");
+        
+    }
 }
+
